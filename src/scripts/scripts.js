1716242d714a03e4ceddc83dea017bc00d90f595
Alex Kurp <kurp96@ya.ru>
2021-05-15 12:50:34 +0300

    Заявки отправляются

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -3,7 +3,7 @@ let mobileMenu = document.querySelector(".mobile-menu");
 let popupMenu = document.querySelector(".menu_popup");
 let popupMobileMenu = document.querySelector(".mobile-menu_popup")
 let modal = document.querySelector(".modal");
-menu.addEventListener("click", function(e){
+menu.addEventListener("click", function (e) {
   e.preventDefault()
   menu.classList.toggle("main-menu__link_active");
   mobileMenu.classList.toggle("main-menu__link_active")
@@ -11,7 +11,7 @@ menu.addEventListener("click", function(e){
   popupMobileMenu.classList.toggle("main-menu__link_active");
   modal.classList.toggle("modal_active");
 });
-mobileMenu.addEventListener("click", function(e){
+mobileMenu.addEventListener("click", function (e) {
   e.preventDefault()
   mobileMenu.classList.toggle("main-menu__link_active");
   menu.classList.toggle("main-menu__link_active");
@@ -20,7 +20,7 @@ mobileMenu.addEventListener("click", function(e){
   modal.classList.toggle("modal_active");
 
 });
-popupMenu.addEventListener("click", function(e){
+popupMenu.addEventListener("click", function (e) {
   e.preventDefault()
   popupMenu.classList.toggle("main-menu__link_active")
   menu.classList.toggle("main-menu__link_active");
@@ -28,7 +28,7 @@ popupMenu.addEventListener("click", function(e){
   popupMobileMenu.classList.toggle("main-menu__link_active");
   modal.classList.toggle("modal_active");
 });
-popupMobileMenu.addEventListener("click", function(e){
+popupMobileMenu.addEventListener("click", function (e) {
   e.preventDefault()
   popupMobileMenu.classList.toggle("main-menu__link_active");
   popupMenu.classList.toggle("main-menu__link_active")
@@ -39,8 +39,8 @@ popupMobileMenu.addEventListener("click", function(e){
 
 // Переходы по ссылкам
 link = document.querySelectorAll(".modal__link");
-link.forEach(function(item){
-  item.addEventListener("click", function(){
+link.forEach(function (item) {
+  item.addEventListener("click", function () {
     popupMobileMenu.classList.toggle("main-menu__link_active");
     popupMenu.classList.toggle("main-menu__link_active")
     menu.classList.toggle("main-menu__link_active");
@@ -53,50 +53,50 @@ link.forEach(function(item){
 let getCall = document.querySelectorAll('.getcall');
 let callBack = document.querySelector('.callback');
 let callBackClose = document.querySelector(".callback__close__span");
-getCall.forEach(function(item){
-  item.addEventListener("click", function(){
+getCall.forEach(function (item) {
+  item.addEventListener("click", function () {
     callBack.classList.toggle("callback_active");
   });
 });
-callBackClose.addEventListener("click", function(){
+callBackClose.addEventListener("click", function () {
   callBack.classList.toggle("callback_active");
 });
 
-let swiper = new Swiper('.reason-slider',{
-    slidesPerView: 3,
-    loop: true,
-    navigation: {
-        prevEl: '.reason-arrow_left',
-        nextEl: '.reason-arrow_right',
-      },
-      breakpoints: {
-        320: {
-          slidesPerView: 1
-        },
-        576:{
-          slidesPerView: 2
-        },
-        991:{
-          slidesPerView: 3,
-          spaceBetween: 20
-        },
-        1200:{
-          spaceBetween: 10
-        }
-      }
+let swiper = new Swiper('.reason-slider', {
+  slidesPerView: 3,
+  loop: true,
+  navigation: {
+    prevEl: '.reason-arrow_left',
+    nextEl: '.reason-arrow_right',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1
+    },
+    576: {
+      slidesPerView: 2
+    },
+    991: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    1200: {
+      spaceBetween: 10
+    }
+  }
 });
-let swiper2 = new Swiper('.gallery-container',{
+let swiper2 = new Swiper('.gallery-container', {
   slidesPerView: 1,
   loop: true,
   navigation: {
-      prevEl: '.gallery-arrow_left',
-      nextEl: '.gallery-arrow_right',
-    },
+    prevEl: '.gallery-arrow_left',
+    nextEl: '.gallery-arrow_right',
+  },
 });
-let swiper3 = new Swiper('.steps-slider',{
+let swiper3 = new Swiper('.steps-slider', {
   slidesPerView: 3,
   loop: true,
-  navigation:{
+  navigation: {
     prevEl: '.steps-arrow_left',
     nextEl: '.steps-arrow_right',
   },
@@ -104,28 +104,27 @@ let swiper3 = new Swiper('.steps-slider',{
     320: {
       slidesPerView: 1
     },
-    576:{
+    576: {
       slidesPerView: 2
     },
-    991:{
+    991: {
       slidesPerView: 3
     }
   }
 });
 const video = document.querySelectorAll(".opportunity-video")
 
-let generateUrl = function(id)
-{
+let generateUrl = function (id) {
   let query = '?rel=0&showinfo=0&autoplay=1';
-  return 'https://youtube.com/embed/'+id+query;
+  return 'https://youtube.com/embed/' + id + query;
 }
-video.forEach((el)=>{
+video.forEach((el) => {
   let videoHref = el.dataset.video;
   let deletedLenght = 'https://youtu.be/'.length;
   let videoId = videoHref.substring(deletedLenght, videoHref.lenght);
   let img = el.querySelector("opportunity-img");
 
-  el.addEventListener('click', function(){
+  el.addEventListener('click', function () {
     let iFrame = createFrame(videoId);
     el.querySelector('.opportunity-img').remove();
     el.appendChild(iFrame);
@@ -134,8 +133,7 @@ video.forEach((el)=>{
   })
 });;
 
-let createFrame = function(id)
-{
+let createFrame = function (id) {
   let iFrame = document.createElement('iframe');
   iFrame.setAttribute('állowfullscreen', '');
   iFrame.setAttribute('állow', 'autoplay', 'encrypted-media');
@@ -155,23 +153,22 @@ valueSelect.addEventListener('change', function () {
   });
 });
 
-tabsBtn.forEach(function(item){
-  item.addEventListener('click', function(){
+tabsBtn.forEach(function (item) {
+  item.addEventListener('click', function () {
     let currentBtn = item;
     let tabId = currentBtn.getAttribute("data-tab");
     let currentTab = document.querySelector(tabId);
-    if( ! currentBtn.classList.contains("infrastructure-item_active"))
-    {
-      tabsBtn.forEach(function(item){
+    if (!currentBtn.classList.contains("infrastructure-item_active")) {
+      tabsBtn.forEach(function (item) {
         item.classList.remove('infrastructure-item_active');
       });
-      tabsItems.forEach(function(item){
+      tabsItems.forEach(function (item) {
         item.classList.remove('infrastructure-item__content_active');
       });
       currentBtn.classList.add('infrastructure-item_active');
       currentTab.classList.add("infrastructure-item__content_active");
     }
-    
+
   });
 });
 document.querySelector(".infrastructure-item").click();
@@ -180,17 +177,16 @@ document.querySelector(".infrastructure-item").click();
 // Табы планировки квартир
 const planBtn = document.querySelectorAll(".plan-tab ");
 const planItem = document.querySelectorAll(".plan__content");
-planBtn.forEach(function(item){
-  item.addEventListener("click", function(){
+planBtn.forEach(function (item) {
+  item.addEventListener("click", function () {
     let currentBtn = item;
     let planId = currentBtn.getAttribute("data-plan");
     let currentPlan = document.querySelector(planId);
-    if(!currentBtn.classList.contains(".plan-tab_active"))
-    {
-      planBtn.forEach(function(item){
+    if (!currentBtn.classList.contains(".plan-tab_active")) {
+      planBtn.forEach(function (item) {
         item.classList.remove('plan-tab_active');
       });
-      planItem.forEach(function(item){
+      planItem.forEach(function (item) {
         item.classList.remove("plan__content_active");
       });
       currentBtn.classList.add("plan-tab_active");
@@ -201,3 +197,118 @@ planBtn.forEach(function(item){
 // simplebar
 new SimpleBar(document.querySelector('.infrastructure-menu-block'));
 new SimpleBar(document.querySelector('.infrastructure-content'));
+
+//jquery
+jQuery(function ($) {
+  $('.phone').mask('+7 (999) 999-99-99');
+  $('.main-form').validate({
+    errorClass: 'invalid',
+    errorElement: 'label',
+    rules: {
+      username: {
+        required: true
+      },
+      userphone: {
+        required: true
+      }
+    },
+    messages:
+    {
+      username:
+      {
+        required: ''
+      },
+      userphone:
+      {
+        required: ''
+      }
+    },
+    submitHandler: function () {
+      event.preventDefault();
+      $.ajax({
+        url: 'mailer/mail.php',
+        type: 'POST',
+        data: $('.main-form').serialize(),
+        success: function (data) {
+          $('.main-form').trigger('reset');
+        }
+      });
+    }
+  });
+  $('.exclusive-form').validate({
+    errorClass: 'invalid',
+    errorElement: 'label',
+    rules: {
+      username: {
+        required: true
+      },
+      userphone: {
+        required: true
+      }
+    },
+    messages:
+    {
+      username:
+      {
+        required: ''
+      },
+      userphone:
+      {
+        required: ''
+      }
+    },
+    submitHandler: function () {
+      event.preventDefault();
+      $.ajax({
+        url: 'mailer/mail.php',
+        type: 'POST',
+        data: $('.exclusive-form').serialize(),
+        success: function (data) {
+          $('.exclusive-form').trigger('reset');
+        }
+      });
+    }
+  });
+  $('.callback__form').validate({
+    errorClass: 'invalid',
+    errorElement: 'label',
+    rules: {
+      username: {
+        required: true
+      },
+      userphone: {
+        required: true
+      },
+      modalCheckbox:
+      {
+        required: true
+      }
+    },
+    messages:
+    {
+      username:
+      {
+        required: ''
+      },
+      userphone:
+      {
+        required: ''
+      },
+      modalCheckbox:
+      {
+        required: 'Что?'
+      }
+    },
+    submitHandler: function () {
+      event.preventDefault();
+      $.ajax({
+        url: 'mailer/mail2.php',
+        type: 'POST',
+        data: $('.callback__form').serialize(),
+        success: function (data) {
+          $('.callback__form').trigger('reset');
+        }
+      });
+    }
+  });
+});
